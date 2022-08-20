16c97970453058d7cb07e957678335990ff00c2b
River <riveriswild@icloud.com>
2022-08-21 02:19:30 +0300

    Add urls

--- chat_sockets/urls.py
+++ chat_sockets/urls.py
@@ -18,6 +18,9 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
+from .views import main_view
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', main_view, name="main")
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
