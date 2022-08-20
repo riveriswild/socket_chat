a33adb9d7aba4e2e4a519d7589c11eba618bcf12
River <riveriswild@icloud.com>
2022-08-20 20:28:34 +0300

    Add models

--- chat_messages/models.py
+++ chat_messages/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from chats.models import Chat
 
-# Create your models here.
+class ChatMessage(models.Model):
+    chat = models.ForeignKey(Chat, on_delete=models.CASCADE, related_name='messages')
+    nick = models.CharField(max_length=120)
+    text = models.TextField()
+    
+    def __str__(self):
+        return f"{self.chat.room_name}-{self.nick}"
--- chats/models.py
+++ chats/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Chat(models.Model):
+    room_name = models.CharField(max_length=120)
+    
+    def __str__(self):
+        return str(self.room_name)
+    
+    def get_messages(self):
+        return self.messages.all()  # get all chat messages related to this room
+    
