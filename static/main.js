d0936e992a0b141bbaa84ba5304c75a3fc7a7ab2
River <riveriswild@icloud.com>
2022-08-21 02:38:29 +0300

    Add user entered/left notifications

--- static/main.js
+++ static/main.js
@@ -6,17 +6,37 @@ const messagesBox = document.getElementById('messages-box')
 const messageInput = document.getElementById('message-input')
 const sendBtn = document.getElementById('send-btn')
 
-socket.on('welcome', msg => {
-    console.log(msg)
-})
+// socket.on('welcome', msg => {
+//     console.log(msg)
+// })
+
+const handleAlerts = (msg, type) => {
+    alertBox.innerHTML = `
+        <div class="alert alert-${type}" role="alert">
+            ${msg}
+        </div>
+    `
+    setTimeout(() => {
+        alertBox.innerHTML = ""
+    }, 5000)
+}
 socket.on('welcome2', msg => {
-    console.log(msg)
+    handleAlerts(msg, 'primary')
+})
+
+socket.on('byebye', msg => {
+    handleAlerts(msg, 'danger')
 })
 
 sendBtn.addEventListener('click', () => {
     const message = messageInput.value
+    message.value = ""
     console.log(message)
 
     socket.emit('message', message)
+})
+
+socket.on('messageToClients', msg => {
+    console.log(msg)
 })
 
